835d5f614c9653c94786538b741bd1186729218c
lolla-sai <80596780+lolla-sai@users.noreply.github.com>
2022-11-05 14:38:05 +0530

    Limited Github results to 8

--- pages/api/github.js
+++ pages/api/github.js
@@ -1,7 +1,7 @@
 var BASE_URL = new URL("https://api.github.com/search/repositories");
 
 export default async function handler(req, res) {
-    let { keyword, max_results = 15 } = req.query;
+    let { keyword, max_results = 8 } = req.query;
 
     BASE_URL.searchParams.set("q", keyword);
     BASE_URL.searchParams.set("per_page", max_results);
